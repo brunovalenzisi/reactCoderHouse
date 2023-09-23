5d29aecd5bcdaa1c6add15b7746baecc3db814c7
brunovalenzisi <130124652+brunovalenzisi@users.noreply.github.com>
2023-09-23 14:53:35 -0300

    Comienzo del proyecto

---
+++ reactCoderHouse/src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div >
+    <NavBar/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ reactCoderHouse/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <>
+      <div className="flex justify-center py-3 bg-blue-400">
+        <nav>
+          <h1 className="text-3xl font-bold text-white">Barra de navegacion</h1>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
